bf2717b45f5416610bfa00ee947f848bb95e8edf
Mads Lundt <madslundt@live.dk>
2017-05-17 11:10:20 +0200

    Gitignore

--- node_modules/antd/lib/layout/layout.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import React from 'react';
-export interface BasicProps {
-    style?: React.CSSProperties;
-    prefixCls?: string;
-    className?: string;
-}
-declare const Layout: any;
-export default Layout;
